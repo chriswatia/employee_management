5fa3c800ec56dce2affc25b3ede2914aa2d90e66
chriswatia <chriswatia@gmail.com>
2022-11-23 13:27:15 +0300

    Created Employee Entity

--- src/main/java/com/warriors/employee/model/Employee.java
+++ src/main/java/com/warriors/employee/model/Employee.java
@@ -1,4 +1,45 @@
 package com.warriors.employee.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String email;
+
+    public Employee( ){
+    }
+
+    public Employee(String firstname, String middlename, String lastname, String email) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
