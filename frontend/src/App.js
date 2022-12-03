aca605ae16bc7718bb127240beb6aabacea7ffa9
Chris Watia <chriswatia@gmail.com>
2022-12-03 17:07:34 +0300

    Implemented react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 
 function App() {
   return (
-    <div className="container-fluid px-4">
-      <ListEmployeeComponent/>
+    <div>
+      <HeaderComponent />
+      <div className="container-fluid">        
+        <ListEmployeeComponent />        
+      </div>
+      <FooterComponent />
     </div>
+
   );
 }
 
